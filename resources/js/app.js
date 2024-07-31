1083dd6dd098b4415ca45b4e65ff1192f400a30e
Raza <raza.khan@reavertechnologies.com>
2024-07-31 15:28:20 +0500

    first commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 
 import './bootstrap';
+import '../sass/app.scss'; 
 import 'laravel-datatables-vite';
 
 import '../assets/js/scripts.bundle.js';
--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: '/public/',
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/js/app.js'],
             refresh: true,
         }),
     ],
