0320efb751e6dff98c995533cd538d5e49caa2a4
Raza <raza.khan@reavertechnologies.com>
2024-07-31 17:13:18 +0500

    first commit

--- routes/api.php
+++ routes/api.php
@@ -15,10 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['middleware' => 'cors'], function () {
-    Route::get('/webhook', [SalesController::class, 'salesDataWebHook'])->name('webhook');
-});
 
+Route::get('/webhook', [SalesController::class, 'salesDataWebHook'])->name('webhook');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
