39cb25512a2f9664e099c57561d387df15c711fe
Raza <raza.khan@reavertechnologies.com>
2024-07-31 13:19:34 +0500

    first commit

--- resources/views/layouts/includes/footer.blade.php
+++ resources/views/layouts/includes/footer.blade.php
@@ -1,8 +1,1 @@
-{{-- <script src="{{ asset('js/jquery/jquery.min.js?id=3242') }}"></script> --}}
-
-{{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
-
-
-{{-- <script src="{{ asset('./public/js/app.js') }}" defer></script> --}}
-
 @stack('scripts')
