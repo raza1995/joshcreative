e803cb33f88de6ab067d59d31b5f21d967ebafe7
Raza <raza.khan@reavertechnologies.com>
2024-08-01 18:20:20 +0500

    updates

--- resources/views/sales/index.blade.php
+++ resources/views/sales/index.blade.php
@@ -3,20 +3,20 @@
 @section('content')
 
 
-<div class="row">
-    <div class="col-12">
-        <div class="container">
-        <div class="card">
+<div class="row ">
 
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-md-12">
-                        {!! $dataTable->table(['class' => 'table table-bordered table-striped dataTable dtr-inline custom-css-datatable']) !!}
+        <div class="container">
+            <div class="card">
+                <div class="card-body">
+                  
+                        <div class="col-md-12">
+                            {!! $dataTable->table(['class' => 'table table-bordered']) !!}
+                        </div>
+                  
                 </div>
             </div>
         </div>
-    </div>
-    </div>
+   
 </div>
 @endsection
     @push('scripts')
