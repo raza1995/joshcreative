eb884d8c12db0ceda486a49760685b5eb204c547
Raza <raza.khan@reavertechnologies.com>
2024-08-05 15:37:02 +0500

    updates

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -8,7 +8,7 @@ use App\Models\SalesMeta;
 class Sale extends Model
 {
     use HasFactory;
-    protected $fillable = ['project_id', 'user_id', 'salesname','email', 'ip_address', 'utm_source', 'total_amount', 'earned_commission', 'created_at', 'updated_at','dj_user_id', 'price','purchase_count'];
+    protected $fillable = ['project_id', 'user_id', 'salesname','email', 'ip_address', 'utm_source', 'total_amount', 'earned_commission', 'created_at', 'updated_at','dj_user_id', 'price','purchase_count','product_id','status', 'promo_code', 'sales_id'];
     
     public function meta()
     {
