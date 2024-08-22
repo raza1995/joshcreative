907e69829be9574a9c9c507774657526260de648
Raza <razakkhanafridi1995@gmail.com>
2024-08-23 00:52:47 +0500

    added coins logs

--- app/Console/Commands/RefreshAnalyticsCache.php
+++ app/Console/Commands/RefreshAnalyticsCache.php
@@ -24,19 +24,19 @@ class RefreshAnalyticsCache extends Command
 
         // Fetch and cache the user journey data
         $userJourneys = $this->analyticsService->fetchUserJourneys($baseUrl);
-        Cache::put('userJourneys', $userJourneys, 60);
+        Cache::put('userJourneys', $userJourneys);
 
         // Create and cache the journey map
         $journeyMap = $this->analyticsService->createJourneyMap($userJourneys);
-        Cache::put('journeyMap', $journeyMap, 60);
+        Cache::put('journeyMap', $journeyMap);
 
         // Calculate and cache the metrics
         $metrics = $this->analyticsService->calculateMetrics($userJourneys, $journeyMap);
-        Cache::put('metrics', $metrics, 60);
+        Cache::put('metrics', $metrics);
 
         // Segment and cache the user data
         $segments = $this->analyticsService->segmentUsers($userJourneys);
-        Cache::put('userSegments', $segments, 60);
+        Cache::put('userSegments', $segments);
 
         $this->info('Analytics cache refreshed successfully.');
     }
