47c5e65468bee8377b12a434e475e62636613c5e
raza935552 <143720746+raza935552@users.noreply.github.com>
2025-02-07 13:47:25 +0500

    updates

--- app/Http/Controllers/SlackController.php
+++ app/Http/Controllers/SlackController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Services\OpenAIService;
 use App\Services\SlackService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
