2682fcf1809f1df1fef07559f71658e361266f82
raza935552 <143720746+raza935552@users.noreply.github.com>
2025-02-08 01:44:03 +0500

    updates

--- app/Services/OpenAIService.php
+++ app/Services/OpenAIService.php
@@ -178,7 +178,7 @@ class OpenAIService
         }
     
         // Optional: Detect if an email address is specified
-        if (preg_match('/\bopen email from\s+([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})\b/i', $query, $matches)) {
+        if (preg_match('/\bopen email from\s+<mailto:([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})\|.*?>/i', $query, $matches)) {
             $intent['openEmailFrom'] = $matches[1];
             Log::info('Detected open email from request.', [
                 'query' => $query,
