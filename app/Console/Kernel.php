7db84d9ba7e44d9643f30926e3500f11fb7d64e7
Raza <raza.khan@reavertechnologies.com>
2025-06-02 22:10:45 +0500

    updated applportal

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
 
-        $schedule->command('shopify:fetch-orders-mycolean')->everySixHours();
+        // $schedule->command('shopify:fetch-orders-mycolean')->everySixHours();
         // $schedule->command('inspire')->hourly();
         // $schedule->command('cache:refresh-analytics')->everyThreeHours();
         // $schedule->command('gmail:process-emails-updated')->everyThreeHours();
@@ -25,11 +25,11 @@ class Kernel extends ConsoleKernel
         // $schedule->command('emails:fetch')->everyMinute();
         // $schedule->command('emails:fetch-unread')->everyFiveMinutes();
         // $schedule->command('emails:process')->everyFiveMinutes();
-        $schedule->command('facebook:sync-daily')->dailyAt('03:00'); // 3 AM daily
+        $schedule->command('facebook:sync-daily')->dailyAt('03:00'); 
 
         $schedule->job(new ExportFacebookAdsToGoogleSheetJob('daily'))->dailyAt('06:00');
-    $schedule->job(new ExportFacebookAdsToGoogleSheetJob('weekly'))->weeklyOn(1, '07:00'); // every Monday
-    $schedule->job(new ExportFacebookAdsToGoogleSheetJob('monthly'))->monthlyOn(1, '08:00'); // 1st day of month
+        $schedule->job(new ExportFacebookAdsToGoogleSheetJob('weekly'))->weeklyOn(1, '07:00'); // every Monday
+     $schedule->job(new ExportFacebookAdsToGoogleSheetJob('monthly'))->monthlyOn(1, '08:00'); // 1st day of month
         // $schedule->call(function () {
         //     app(SlackService::class)->sendMessage("⏰ Automated check-in from Mycolean AI.");
         // })->everyFiveMinutes();
