55bc46696be343998b865f90c8efbb9c7a85bde2
raza <razakkhanafridi@gmail.com>
2025-07-28 06:19:52 +0500

    updates

--- app/Mail/FacebookAdInsightsEmail.php
+++ app/Mail/FacebookAdInsightsEmail.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
@@ -13,27 +12,41 @@ class FacebookAdInsightsEmail extends Mailable
 {
     use Queueable, SerializesModels;
 
+    /** @var Collection<array|object> */
     public Collection $ads;
+
     public Carbon $startDate;
     public Carbon $endDate;
+    public int    $windowDays;   // <-- NEW
 
-    public function __construct(Collection $ads, Carbon $startDate, Carbon $endDate)
+    /**
+     * @param Collection $ads          Aggregated ads from the command
+     * @param Carbon     $startDate    Beginning of the "current" window
+     * @param Carbon     $endDate      End of the "current" window
+     * @param int        $windowDays   Size of each comparison window (e.g. 30)
+     */
+    public function __construct(Collection $ads, Carbon $startDate, Carbon $endDate, int $windowDays = 30)
     {
-        $this->ads = $ads;
-        $this->startDate = $startDate;
-        $this->endDate = $endDate;
+        $this->ads        = $ads;
+        $this->startDate  = $startDate;
+        $this->endDate    = $endDate;
+        $this->windowDays = $windowDays;
     }
 
     public function build()
     {
-     
-        return $this->subject('Facebook Ad Insights: Top 10 Ads ' . $this->startDate->format('M j') . '–' . $this->endDate->format('M j'))
-                    ->view('emails.facebook_ad_insights')
-                    ->with([
-                        'ads' => $this->ads,
-                        'start' => $this->startDate,
-                        'end' => $this->endDate,
-                    ]);
+        $label = "{$this->windowDays}‑day";
+
+        return $this->subject(
+                    "Facebook Ad Insights ({$label}) " .
+                    $this->startDate->format('M j') . ' – ' . $this->endDate->format('M j')
+                )
+                ->view('emails.facebook_ad_insights')
+                ->with([
+                    'ads'        => $this->ads,
+                    'start'      => $this->startDate,
+                    'end'        => $this->endDate,
+                    'windowDays' => $this->windowDays,   // <-- pass to Blade
+                ]);
     }
 }
-
